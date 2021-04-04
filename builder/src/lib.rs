67b209982dba48673f2ee2dd1827bfbcdfdc6e24
rofi <0x.rofi@gmail.com>
2021-04-05 04:48:22 +0900

    implements: builder, seq

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,191 @@
 use proc_macro::TokenStream;
+use proc_macro2::TokenStream as TStream2;
+use quote::{quote, ToTokens};
+use syn::{parse_macro_input, Data, DeriveInput, Field, Ident, Lit, Meta, NestedMeta, Type};
 
-#[proc_macro_derive(Builder)]
+#[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input = parse_macro_input!(input as DeriveInput);
 
-    unimplemented!()
+    let name = input.ident;
+
+    let builder_name = Ident::new(&format!("{}Builder", name), name.span());
+
+    let fields = match input.data {
+        Data::Struct(ref data) => &data.fields,
+        Data::Enum(_) | Data::Union(_) => {
+            unimplemented!()
+        }
+    };
+
+    fn get_generic_type<'a>(ty: &'a Type, outer: &'static str) -> Option<&'a Type> {
+        match ty {
+            Type::Path(path) => {
+                if path.qself.is_some() {
+                    return None;
+                }
+
+                if path.path.leading_colon.is_some() {
+                    return None;
+                }
+
+                if path.path.segments.len() != 1 {
+                    return None;
+                }
+
+                let path_seg = path.path.segments.first().unwrap();
+                if path_seg.ident.to_string() != outer {
+                    return None;
+                }
+
+                let ab = match path_seg.arguments {
+                    syn::PathArguments::AngleBracketed(ref ab) => ab,
+                    syn::PathArguments::None | syn::PathArguments::Parenthesized(_) => return None,
+                };
+
+                if ab.args.len() != 1 {
+                    return None;
+                }
+
+                match ab.args.first().unwrap() {
+                    syn::GenericArgument::Type(gty) => Some(gty),
+                    syn::GenericArgument::Lifetime(_)
+                    | syn::GenericArgument::Binding(_)
+                    | syn::GenericArgument::Constraint(_)
+                    | syn::GenericArgument::Const(_) => None,
+                }
+            }
+            _ => None,
+        }
+    }
+    fn get_option_type(ty: &Type) -> Option<&Type> {
+        return get_generic_type(ty, "Option");
+    }
+    fn has_builder_attr(field: &Field) -> Result<Option<(Ident, &Type)>, TStream2> {
+        for attr in &field.attrs {
+            if let Ok(Meta::List(outer_meta)) = attr.parse_meta() {
+                if outer_meta.path.to_token_stream().to_string() == "builder" {
+                    if outer_meta.nested.len() == 1 {
+                        if let NestedMeta::Meta(Meta::NameValue(meta_nv)) =
+                            outer_meta.nested.first().unwrap()
+                        {
+                            if meta_nv.path.to_token_stream().to_string() == "each" {
+                                if let Lit::Str(lit) = &meta_nv.lit {
+                                    if let Some(ty) = get_generic_type(&field.ty, "Vec") {
+                                        return Ok(Some((
+                                            Ident::new(&lit.value(), lit.span()),
+                                            ty,
+                                        )));
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    return Err(syn::Error::new_spanned(
+                        outer_meta.to_token_stream(),
+                        r#"expected `builder(each = "...")`"#,
+                    )
+                    .to_compile_error());
+                }
+            }
+        }
+        return Ok(None);
+    }
+
+    if let Some(err) = fields.iter().find_map(|f| has_builder_attr(f).err()) {
+        return err.into();
+    }
+
+    let builder_field = fields.iter().map(|f| {
+        let name = &f.ident;
+        let ty = &f.ty;
+
+        if has_builder_attr(f).unwrap().is_some() || get_option_type(ty).is_some() {
+            quote! {
+                #name: #ty,
+            }
+        } else {
+            quote! {
+                #name: std::option::Option<#ty>,
+            }
+        }
+    });
+    let builder_init = fields.iter().map(|f| {
+        let name = &f.ident;
+
+        if has_builder_attr(f).unwrap().is_some() {
+            quote! {
+                #name: std::vec::Vec::new(),
+            }
+        } else {
+            quote! {
+                #name: std::option::Option::<_>::None,
+            }
+        }
+    });
+    let builder_method = fields.iter().map(|f| {
+        let name = &f.ident;
+        let ty = &f.ty;
+
+        if let Some((lit, gty)) = has_builder_attr(f).unwrap() {
+            quote! {
+                fn #lit(&mut self, #name: #gty) -> &mut Self {
+                    self.#name.push(#name);
+                    self
+                }
+            }
+        } else if let Some(gty) = get_option_type(ty) {
+            quote! {
+                fn #name(&mut self, #name: #gty) -> &mut Self {
+                    self.#name = std::option::Option::<_>::Some(#name);
+                    self
+                }
+            }
+        } else {
+            quote! {
+                fn #name(&mut self, #name: #ty) -> &mut Self {
+                    self.#name = std::option::Option::<_>::Some(#name);
+                    self
+                }
+            }
+        }
+    });
+    let builder_build = fields.iter().map(|f| {
+        let name = &f.ident;
+        let ty = &f.ty;
+
+        if has_builder_attr(f).unwrap().is_some() || get_option_type(ty).is_some() {
+            quote! {
+                #name: self.#name.clone(),
+            }
+        } else {
+            quote! {
+                #name: self.#name.as_ref().ok_or("missing".to_string())?.clone(),
+            }
+        }
+    });
+
+    (quote! {
+        pub struct #builder_name {
+            #(#builder_field)*
+        }
+
+        impl #builder_name {
+            #(#builder_method)*
+
+            fn build(&mut self) -> std::result::Result::<#name, std::boxed::Box<dyn std::error::Error>> {
+                std::result::Result::<_, _>::Ok(#name {
+                    #(#builder_build)*
+                })
+            }
+        }
+
+        impl #name {
+            pub fn builder() -> #builder_name {
+                #builder_name {
+                    #(#builder_init)*
+                }
+            }
+        }
+    }).into()
 }
--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,8 +1,205 @@
 use proc_macro::TokenStream;
+use proc_macro2::{Delimiter, Group, Ident, Literal, TokenStream as TStream2, TokenTree};
+use syn::{
+    braced,
+    parse::{Parse, ParseStream},
+    parse_macro_input, LitInt, Result, Token,
+};
+
+struct Seq {
+    ident: Ident,
+    start: i64,
+    end: i64,
+    ts: TStream2,
+}
+
+impl Seq {
+    fn replace_ident(&self, n: i64, ts: TStream2) -> TStream2 {
+        let result =
+            ts.into_iter()
+                .fold((Vec::<TokenTree>::new(), false), |mut acc, tt| match tt {
+                    TokenTree::Group(group) => {
+                        if acc.1 {
+                            unimplemented!()
+                        } else {
+                            acc.0.push(
+                                Group::new(
+                                    group.delimiter(),
+                                    self.replace_ident(n, group.stream()),
+                                )
+                                .into(),
+                            );
+                            acc
+                        }
+                    }
+
+                    TokenTree::Ident(ref ident) if ident.to_string() == self.ident.to_string() => {
+                        if acc.1 {
+                            let prev = acc.0.pop().unwrap();
+                            acc.0
+                                .push(Ident::new(&format!("{}{}", prev, n), prev.span()).into())
+                        } else {
+                            acc.0.push(Literal::i64_unsuffixed(n).into())
+                        }
+
+                        (acc.0, false)
+                    }
+
+                    TokenTree::Ident(_) => {
+                        if acc.1 {
+                            unimplemented!()
+                        }
+                        acc.0.push(tt);
+                        acc
+                    }
+
+                    TokenTree::Punct(ref p) if p.to_string() == "#" => {
+                        if acc.1 {
+                            unimplemented!()
+                        }
+                        (acc.0, true)
+                    }
+
+                    TokenTree::Punct(_) | TokenTree::Literal(_) => {
+                        if acc.1 {
+                            unimplemented!()
+                        }
+                        acc.0.push(tt);
+                        acc
+                    }
+                });
+        if result.1 {
+            unimplemented!()
+        }
+
+        result.0.into_iter().collect()
+    }
+
+    fn has_loop(&self) -> bool {
+        self.check_loop(self.ts.clone())
+    }
+
+    fn check_loop(&self, ts: TStream2) -> bool {
+        #[derive(PartialEq, Eq)]
+        enum State {
+            None,
+            Pound,
+            Parenthesis,
+            Complete,
+        }
+
+        ts.into_iter().fold(State::None, |state, tt| match tt {
+            TokenTree::Group(group) => {
+                if state == State::Pound && group.delimiter() == Delimiter::Parenthesis {
+                    State::Parenthesis
+                } else if self.check_loop(group.stream().clone()) {
+                    State::Complete
+                } else {
+                    State::None
+                }
+            }
+            TokenTree::Ident(_) => State::None,
+            TokenTree::Punct(ref p) => match (p.to_string().as_str(), state) {
+                ("#", State::None) => State::Pound,
+                ("*", State::Parenthesis) => State::Complete,
+                _ => State::None,
+            },
+            TokenTree::Literal(_) => State::None,
+        }) == State::Complete
+    }
+
+    fn expand_loop(&self, ts: TStream2) -> TStream2 {
+        ts.into_iter()
+            .fold((None, Vec::<TokenTree>::new()), |mut acc, tt| match tt {
+                TokenTree::Group(ref group) => {
+                    if acc.0.is_some() && group.delimiter() == Delimiter::Parenthesis {
+                        acc.1.extend(
+                            (self.start..self.end)
+                                .map(|n| self.replace_ident(n, group.stream().clone()))
+                                .flatten(),
+                        );
+                        return acc;
+                    }
+
+                    if let Some(prev_tt) = acc.0 {
+                        acc.1.push(prev_tt)
+                    }
+                    acc.1.push(
+                        Group::new(group.delimiter(), self.expand_loop(group.stream())).into(),
+                    );
+
+                    (None, acc.1)
+                }
+                TokenTree::Punct(ref p) => {
+                    if p.to_string() == "#" {
+                        (Some(tt), acc.1)
+                    } else if acc.0.is_some() && p.to_string() == "*" {
+                        (None, acc.1)
+                    } else {
+                        acc.1.push(tt);
+                        (None, acc.1)
+                    }
+                }
+                TokenTree::Ident(_) | TokenTree::Literal(_) => {
+                    acc.1.push(tt);
+                    acc
+                }
+            })
+            .1
+            .into_iter()
+            .collect()
+    }
+}
+
+impl Parse for Seq {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let ident = input.parse()?;
+        input.parse::<Token![in]>()?;
+        let start = input.parse::<LitInt>()?.base10_parse()?;
+
+        let inclusive;
+        if input.peek(Token![..=]) {
+            inclusive = true;
+            input.parse::<Token![..=]>()?;
+        } else {
+            inclusive = false;
+            input.parse::<Token![..]>()?;
+        }
+
+        let mut end = input.parse::<LitInt>()?.base10_parse()?;
+        if inclusive {
+            end += 1;
+        }
+
+        let content;
+        braced!(content in input);
+
+        Ok(Seq {
+            ident,
+            start,
+            end,
+            ts: TStream2::parse(&content)?,
+        })
+    }
+}
+
+impl Into<TokenStream> for Seq {
+    fn into(self) -> TokenStream {
+        if self.has_loop() {
+            self.expand_loop(self.ts.clone()).into()
+        } else {
+            (self.start..self.end)
+                .map(|n| self.replace_ident(n, self.ts.clone()))
+                .flatten()
+                .collect::<TStream2>()
+                .into()
+        }
+    }
+}
 
 #[proc_macro]
 pub fn seq(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let seq = parse_macro_input!(input as Seq);
 
-    unimplemented!()
+    seq.into()
 }
